6da06ddd12c5f17bb8b33330a574110169efb05d
Sasha <sashikers@gmail.com>
2017-12-08 21:28:21 -0800

    added question set

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,56 @@
+var questionSet = [
+	{
+		question: "What is Earth's largest continent?",
+		answers: {
+			a: "Antarctica",
+			b: "Africa",
+			c: "Europe",
+			d: "Asia",
+		},
+		correctAnswer: "d",
+	},
+	{
+		question: "What razor-thin country accounts for more than half of the western coastine of South America?",
+		answers: {
+			a: "Bolivia",
+			b: "Equador",
+			c: "Peru",
+			d: "Chile",
+		},
+		correctAnswer: "d",
+	},
+	{
+		question: "What river runs through Baghdad?",
+		answers: {
+			a: "Jordan",
+			b: "Tigris",
+			c: "Euphrates",
+			d: "Karun",
+		},
+		correctAnswer: "b",
+	},
+	{
+		question: "What country has the most natural lakes?",
+		answers: {
+			a: "Canada",
+			b: "United States",
+			c: "Australia",
+			d: "India",
+		},
+		correctAnswer: "a",
+	},
+	{
+		question: "What is the only sea without any coasts?",
+		answers: {
+			a: "Sargasso Sea",
+			b: "Adriatic Sea",
+			c: "Celebes Sea",
+			d: "Mediterranean Sea",
+		}
+		correctAnswer: "a",
+	}
+]
+
 var quizContainer = $("#quiz");
 // var quizContainer = document.getElementById('quiz');
 var submitButton = $("#submit");
@@ -6,6 +59,13 @@ console.log(quizContainer);
 
 function quizInitiate() {}
 
-function displayResults() {}
+function displayResults() {
+	console.log("hello");
+}
 
 quizInitiate();
+// submitButton.addEventListener("click", displayResults);
+$("#submit").on("click",displayResults());
+
+
+console.log(questionSet);
