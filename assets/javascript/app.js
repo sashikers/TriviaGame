e1f1660ce2df8880baaf4feccbf979ddd752390a
Sasha <sashikers@gmail.com>
2017-12-08 21:04:05 -0800

    basic java setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var quizContainer = $("#quiz");
+// var quizContainer = document.getElementById('quiz');
+var submitButton = $("#submit");
+var resultsContainer = $("#results");
+console.log(quizContainer);
+
+function quizInitiate() {}
+
+function displayResults() {}
+
+quizInitiate();
