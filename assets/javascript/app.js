0e9753b8a58aad3b57fa38640fcf1fc0db0d34bd
Sasha <sashikers@gmail.com>
2017-12-09 21:18:10 -0800

    basic structure done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
 
-const questionSet = [
+var myQuestions = [
 	{
 		question: "What is Earth's largest continent?",
 		answers: {
@@ -53,46 +53,63 @@ const questionSet = [
 	},
 ];
 
-const quizContainer = $("#quiz");
-const submitButton = $("#submit");
-const resultsContainer = $("#results");
-console.log(quizContainer);
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
 
-function quizInitiate(event) {
-	// event.preventDefault();
-	const output = []; 
-	questionSet.forEach(
-		(currentQuestion, questionNumber) => {
-			const answers = [];
-			for (letter in currentQuestion.answers){
-				// answers.push(
-				// 	'<label><input type="radio" name="question${questionNumber}" value ="${letter}">${letter}:${currentQuestion.answers[letter]}</label>'
-				// );
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+	function showQuestions(questions, quizContainer) {
+		var output = [];
+		var answers;
+
+		for(var i = 0; i<questions.length; i++){
+			answers = [];
+
+			for(letter in questions[i].answers){
 				answers.push(
-					'<label><input type="radio" name="question' + questionNumber + '" value="' + letter + '">' + letter + ':' + currentQuestion.answers[letter] + '</label>'
-				);				
+					'<label>'
+						+ '<input type="radio" name="question>' + i + '" value="' + letter + '">'
+						+ letter + ': '
+						+ questions[i].answers[letter]
+					+ '</label>'
+				);
 			}
 
 			output.push(
-				'<div class="question">'+ currentQuestion.question + '</div><div class="answers">' + answers.join("") + '</div>'	
+				'<div class="question">' + questions[i].question + '</div>'
+				+ '<div class="answers">' + answers.join('') + '</div>'
 			);
-		}	
-	);
+		}
 
-	// quizContainer.innerHTML = output.join("");
-	$("#quiz").html(output.join(""));
-	// document.getElementById("quiz").innerHTML = output.join("");
+		quizContainer.innerHTML = output.join('');
+	}
+
+	function showResults(questions, quizContainer, resultsContainer) {
+		var answerContainers = quizContainer.querySelectorAll('.answers');
+
+		var userAnswer = '';
+		var numCorrect = 0; 
+
+		for (var i = 0; i<questions.length; i++){
+			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+			console.log(userAnswer);
+			if(userAnswer === questions[i].correctAnswer) {
+				numCorrect++;
+				answerContainers[i].style.color = 'green';
+			} else {
+				answerContainers[i].style.color = 'red';
+			}
+		}
+
+		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+	}
+
+	showQuestions(questions, quizContainer);
+
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	}
 }
 
-function displayResults(event) {
-	// event.preventDefault();
-	console.log("hello");
-
-}
-
-quizInitiate();
-// submitButton.addEventListener("click", displayResults);
-$("#submit").on("click",displayResults());
-
-
-console.log(questionSet);
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
